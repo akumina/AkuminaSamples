409a191b3ab232b4fb7c6c2a3919e0c8c387b233
Udaiappa Ramachandran <udaiappa@gmail.com>
2018-12-06 21:03:43 -0500

    peoplesync sample added

---
+++ PeopleSyncCustomization/PeopleSync.Customization/CustomFilter.cs
@@ -0,0 +1,38 @@
+﻿using Akumina.PeopleSync.Core;
+using Akumina.PeopleSync.Core.Interfaces;
+using Newtonsoft.Json.Linq;
+
+namespace PeopleSync.Customization
+{
+    public class CustomFilter:ICustomFilter
+    {
+        public dynamic UserFilter(dynamic changes)
+        {
+            ConsoleLogger.ShowInfo("Processing changes from custom library...");
+            var result = new JArray();
+            var items = (JArray)changes;
+            foreach (var item in items)
+            {
+                if (item["givenName"] == null || item["givenName"] != null && string.IsNullOrEmpty(item["givenName"].ToString()))
+                    continue;
+                if (item["surname"] == null || item["surname"] != null && string.IsNullOrEmpty(item["surname"].ToString()))
+                    continue;
+                if (item["accountEnabled"] == null || item["accountEnabled"] != null && item["accountEnabled"].ToString().ToLower() != "true")
+                    continue;
+                if (item["userType"] == null || item["userType"] != null && item["userType"].ToString().ToLower() != "member")
+                    continue;
+                if (item["userPrincipalName"] == null || item["userPrincipalName"] != null && item["userPrincipalName"].ToString().StartsWith("ADMIN."))
+                    continue;
+                if (item["dirsyncenabled"] != null && item["dirsyncenabled"].ToString().Equals("false", System.StringComparison.InvariantCultureIgnoreCase))
+                    continue;
+                result.Add(item);
+            }
+            return result;
+        }
+
+        public dynamic GroupFilter(dynamic changes)
+        {
+            return changes;
+        }
+    }
+}
---
+++ PeopleSyncCustomization/PeopleSync.Customization/CustomSource.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Akumina.PeopleSync.Core;
+using Akumina.PeopleSync.Core.Entities;
+using Akumina.PeopleSync.Core.Interfaces;
+
+namespace PeopleSync.Customization
+{
+    public class CustomSource : ISyncSource
+    {
+        public QueryResult QueryUsers(string resourceSet, string nextLink, ICollection<string> objectClassList, ICollection<string> propertyList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryResult QueryGroups(string resourceSet, string nextLink, ICollection<string> objectClassList, ICollection<string> propertyList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FetchGroupExtensions(string lastExtensionLink, List<KeyValuePair<string, (string CreationDate, string Visibility, string Tags, string Types)>> groupExtensions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> GetData(string objectId, dynamic client, SyncTask.ProcessTask callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, string>> GetAssignedLicenses(string objectId, dynamic client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, string>> GetAppRoleAssignments(string objectId, dynamic client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, string>> GetDirectReports(string objectId, dynamic client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, string>> GetOwnedDevices(string objectId, dynamic client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, string>> GetRegisteredDevices(string objectId, dynamic client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, string>> GetExtendedProperties(string objectId, dynamic client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, string>> GetManager(string objectId, dynamic client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, string>> GetGroupMembers(string objectId, dynamic client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, string>> GetExtendedPropertiesForGroup(string objectId, dynamic client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Dictionary<string, string>> OnPremisesExtensionAttributes(string objectId, dynamic client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
